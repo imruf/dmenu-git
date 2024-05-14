ea7bdc404b70386329d72e504c4554ed1d302443
imrufbd <imrufbd@gmail.com>
2024-05-15 00:21:19 +0600

    color fix

--- config.def.h
+++ config.def.h
@@ -4,19 +4,19 @@
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"monospace:pixelsize=14:antialias=true:autohint=true"
+	"monospace:pixelsize=16:antialias=true:autohint=true"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
-	[SchemeNorm] = { "#bbbbbb", "#222222" },
-	[SchemeSel] = { "#eeeeee", "#005577" },
-	[SchemeMid] = { "#eeeeee", "#770000" },
-	[SchemeSelHighlight] = { "#ffc978", "#005577" },
-	[SchemeNormHighlight] = { "#ffc978", "#222222" },
-	[SchemeOut] = { "#000000", "#00ffff" },
-	[SchemeMid] = { "#eeeeee", "#770000" },
-	[SchemeOutHighlight] = { "#ffc978", "#00ffff" },
+	[SchemeNorm] = { "#ff5874", "#092236" },
+	[SchemeSel] = { "#092236", "#ff5874" },
+	[SchemeMid] = { "#092236", "#ecc48d" },
+	[SchemeSelHighlight] = { "#ecc48d", "#092236" },
+	[SchemeNormHighlight] = { "#ff5874", "#2c3043" },
+	[SchemeOut] = { "#092236", "#ecc48d" },
+	[SchemeMid] = { "#c3ccdc", "#092236" },
+	[SchemeOutHighlight] = { "#092236", "#ff5874" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
 static unsigned int lines      = 0;
